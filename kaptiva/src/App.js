b3323a7531dcc5707e300189671f45546db74340
chuchobaez <carlosbaez472@gmail.com>
2022-08-10 15:39:48 -0400

    start wireframe

--- kaptiva/src/App.js
+++ kaptiva/src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import circle from './assets/img1.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div-lila">
+      <img src={circle}/>
+      <h1 className='titulo'>Un nuevo<br/> modo de<br/> trabajo →</h1>
     </div>
   );
 }
