d5882879d32f432675de854a7b0d6a4f1e8187a1
chuchobaez <carlosbaez472@gmail.com>
2022-08-12 17:15:14 -0400

    sectionUpdate

--- kaptiva/src/App.js
+++ kaptiva/src/App.js
@@ -1,24 +1,27 @@
+import React, {Fragment} from 'react';
 import './App.css';
 import circle from './assets/circle.svg';
 import circle2 from './assets/circle2.svg'
-import photoUno from './assets/img-n1.png';
-import photoDos from './assets/img-n2.png';
-import photoTres from './assets/img-n3.png';
+import photoUno from './assets/img-m1new.png';
+import photoDos from './assets/img-n2new.png';
+import photoTres from './assets/img-n3new.png';
 import star from './assets/star.png';
-import photoS31 from './assets/img-s31.png';
-import photoS32 from './assets/imgs32.png';
-import photoS33 from './assets/imgs33.png';
-import photoS34 from './assets/imgs34.png';
-import photoS35 from './assets/imgs35.png';
-import photoS36 from './assets/imgs36.png';
-import photoEstrella1 from './assets/star1.png'
-import photoCuatroUno from './assets/imgs41.png'
-import photoCuatroDos from './assets/imgs42.png'
-import photoCuatroTres from './assets/imgs43.png'
+import photoS31 from './assets/img-s31new.png';
+import photoS32 from './assets/imgs32new.png';
+import photoS33 from './assets/imgs33new.png';
+import photoS34 from './assets/imgs34new.png';
+import photoS35 from './assets/imgs35new.png';
+import photoS36 from './assets/imgs36new.png';
+import photoEstrella1 from './assets/star1.png';
+import photoCuatroUno from './assets/imgs41new.png';
+import photoCuatroDos from './assets/imgs42new.png';
+import photoCuatroTres from './assets/imgs43new.png';
+import estrella4 from './assets/star2.png'
+
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <div className="div-lila">
         <img className='circle' src={circle} alt="" />   
         <h1 className='titulo'>Un nuevo<br/> modo de<br/> trabajo →</h1>
@@ -57,6 +60,7 @@ function App() {
       </div>
       <div className='div-sec4'>
         <h2 className='sec4'>Socios K</h2>
+        <img className='estrella4' alt="" src={estrella4} />
         <div className='img41'>
           <img alt="" src={photoCuatroUno} />
           <p>Ultimate Lab Agency</p>
@@ -88,7 +92,17 @@ function App() {
           </p>
         </div>
       </div>
-    </div>
+      <div className='div-sec6'>
+        <div className='whatsapp'>
+          <h3>Whastapp</h3>
+          <p>+595 991 667 901</p>
+        </div>
+        <div className='correo'>
+          <h3>Correo</h3>
+          <p>hola@kaptiva.com</p>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
